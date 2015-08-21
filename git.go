d3d685b92e995aab64e6e1e86fc750dd06c2ec34
buckhx <buckhx@gmail.com>
2015-08-21 20:12:54 +0000

    compiles

--- git.go
+++ git.go
@@ -1,56 +1,57 @@
 package git
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
-	"bytes"
 	pathlib "path"
+	"strings"
 
 	"github.com/buckhx/pathutil"
 )
 
-var NOF := []string{""}
+var NOF = []string{}
 
 type Repository struct {
 	Path string
 }
 
-func (repo *Repository) Op(cmd string, flags []string, ...args string) (string, error) {
+func (repo *Repository) Op(cmd string, flags []string, args ...string) (out string, err error) {
 	flags = append(flags, "-C", repo.Path)
-	return Op(cmd, flags, args...)
+	return Operation(cmd, flags, args...)
 }
 
 func (repo *Repository) Init() (err error) {
-	_, err := repo.Op("init", NOF)
+	_, err = repo.Op("init", NOF)
 	return
 }
 
-func Add(paths []string) (err error) {
-	_, err := repo.Op("add", NOF, paths...)
+func (repo *Repository) Add(paths []string) (err error) {
+	_, err = repo.Op("add", NOF, paths...)
 	return
 }
 
-func Commit(msg string) (err error) {
-	_, err := repo.Op("commit", NOF, "-am", msg)
+func (repo *Repository) Commit(msg string) (err error) {
+	_, err = repo.Op("commit", NOF, "-am", msg)
 	return
-	
+
 }
 
-func Push() (err error) {
-	_, err := repo.Op("push", NOF)
+func (repo *Repository) Push() (err error) {
+	_, err = repo.Op("push", NOF)
 	return
 }
 
-func Op(cmd string, flags []string, ...args string) (string, error) {
-	var stderr, stdout bytes.Buffer	
-	args = append([]string{cmd}, args...)
+func Operation(command string, flags []string, args ...string) (string, error) {
+	var stderr, stdout bytes.Buffer
+	args = append([]string{command}, args...)
 	args = append(flags, args...)
 	cmd := exec.Command("git", args...)
 	cmd.Stderr = &stderr
 	cmd.Stdout = &stdout
 	err := cmd.Run()
 	if err != nil {
-		err = fmt.Errorf("%s\n%s\n", stderr.String(), strings.Join(append([]string{"git"}, args...)," "))
+		err = fmt.Errorf("%s\n%s\n", stderr.String(), strings.Join(append([]string{"git"}, args...), " "))
 	}
 	return stdout.String(), err
 }
@@ -59,14 +60,12 @@ func IsRepository(path string) bool {
 	return pathutil.Exists(pathlib.Join(path, ".git"))
 }
 
-func NewRepository(path string) (repo Repository, err error) {
+func NewRepository(path string) (repo *Repository, err error) {
 	if !pathutil.Exists(path) {
-		err := fmt.Errorf("Cannot instantiate Repository because path doesn't exist at %q", path)
-		repo := nil
+		err = fmt.Errorf("Cannot instantiate Repository because path doesn't exist at %q", path)
 	} else {
-		repo := &Repository
+		repo = &Repository{}
 		repo.Path = path
-		err := nil
 	}
-	return 
+	return
 }
